e792a091c9b8703927b9e4b5a8b98fd469dcf921
TERNOX <43003712+TERNOX@users.noreply.github.com>
2020-10-03 16:41:48 +0300

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Text, Box, Image } from "@quarkly/widgets";
+import { Theme, Text, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { Override, Menu, Section, StackItem, Stack } from "@quarkly/components";
 export default (() => {
@@ -39,6 +39,7 @@ export default (() => {
 			</Box>
 		</Section>
 		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Override slot="SectionContent" max-width="1400px" />
 			<Text
 				as="h1"
 				font="--headline1"
@@ -60,68 +61,66 @@ export default (() => {
 				The smartest people work every day to provide the best service and to make our clients happy
 			</Text>
 			<Stack margin-top="40px">
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
+				<StackItem width="20%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/nexoria-min.jpg) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						Rogue-like with cards
 					</Text>
 					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Nathan K. Joe
+						Nexoria: Dungeon Rogue Heroes
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
-						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+						Hardcore roguelike about dungeons with hybrid battles, combining classical turn based combat system and a collectible card game. Collect the best spells and conquer the dungeons!
 					</Text>
 				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://cdn02.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_download_software/SQ_NSwitchDS_TaimumariCompleteEdition.jpg) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
-						Platformer spirited by Mega Man X
+						Platformer inspired by MegaMan X
 					</Text>
 					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						David R. Lema
+						Taimumari
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
 						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
 					</Text>
 				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/assets/images/legend.jpg) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						Arcade
 					</Text>
 					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Janet E. Morey
+						Legend of Himari
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
 						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
 					</Text>
 				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://cdn02.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_download_software/SQ_NSwitchDS_InVert.jpg) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						Puzzle platformer
 					</Text>
 					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Claire R. Peery
-						<br />
+						IN-VERT
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
 						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
 					</Text>
 				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/assets/images/bullet_big.jpg) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						Shut'em'up with rythm
 					</Text>
 					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Claire R. Peery
-						<br />
+						Bullet Beat
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
 						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
@@ -129,26 +128,95 @@ export default (() => {
 				</StackItem>
 			</Stack>
 		</Section>
-		<Section text-align="center" padding="100px 0">
-			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0">
-				The headline about us
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Override slot="SectionContent" max-width="1400px" />
+			<Text
+				as="h1"
+				font="--headline1"
+				md-font="--headline2"
+				max-width="520px"
+				margin="0 auto 5px auto"
+				text-align="center"
+			>
+				Published
 			</Text>
-			<Text as="p" font="--lead" margin="20px 0">
-				Tell what your business is about, what you do and how you do it.
-		Write an engaging story describing your work experience and how
-		passionate you are about what you do.
+			<Text
+				as="p"
+				color="--grey"
+				max-width="520px"
+				margin="0 auto"
+				text-align="center"
+				font="--lead"
+			>
+				The smartest people work every day to provide the best service and to make our clients happy
 			</Text>
-			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around">
-				<Box padding="10px">
-					<Image src="https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
-				</Box>
-				<Box padding="10px">
-					<Image src="https://images.unsplash.com/photo-1503341960582-b45751874cf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" width="320px" max-width="100%" />
-				</Box>
-				<Box padding="10px">
-					<Image src="https://images.unsplash.com/photo-1503342394128-c104d54dba01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
-				</Box>
-			</Box>
+			<Stack margin-top="40px">
+				<StackItem width="20%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/nexoria-min.jpg) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Rogue-like with cards
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Nexoria: Dungeon Rogue Heroes
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						Hardcore roguelike about dungeons with hybrid battles, combining classical turn based combat system and a collectible card game. Collect the best spells and conquer the dungeons!
+					</Text>
+				</StackItem>
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://cdn02.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_download_software/SQ_NSwitchDS_TaimumariCompleteEdition.jpg) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Platformer inspired by MegaMan X
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Taimumari
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/assets/images/legend.jpg) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Arcade
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Legend of Himari
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://cdn02.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_download_software/SQ_NSwitchDS_InVert.jpg) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Puzzle platformer
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						IN-VERT
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="20%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(http://ternoxgames.com/assets/images/bullet_big.jpg) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Shut'em'up with rythm
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Bullet Beat
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+			</Stack>
 		</Section>
 	</Theme>;
 });
